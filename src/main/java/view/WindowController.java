c1912a8489b196a071c1f52a2348bf54c2902bdd
Orel Matatov <orel.matatov@gmail.com>
2023-05-10 16:25:28 +0300

    Establish observer design pattern

---
+++ src/main/java/view/WindowController.java
@@ -0,0 +1,10 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.Observable;
+
+public class WindowController extends Observable {
+
+}
---
+++ src/main/java/viewModel/viewModel.java
@@ -0,0 +1,24 @@
+package viewModel;
+
+import model.Model;
+import view.WindowController;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class viewModel implements Observer {
+    Model m;
+    WindowController wc;
+
+    public viewModel(Model m, WindowController wc){
+        this.m = m;
+        this.wc = wc;
+        m.addObserver(this);
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
