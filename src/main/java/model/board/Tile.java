6736c54901fcda3f48c946ffe10966659fe57d72
Orel Matatov <orel.matatov@gmail.com>
2023-05-27 19:33:08 +0300

    Update Tile.java

--- src/main/java/model/board/Tile.java
+++ src/main/java/model/board/Tile.java
@@ -1,24 +1,18 @@
+
 package model.board;
 
-
 import java.util.Objects;
-
+import java.util.Random;
 
 public class Tile {
+
     public final char letter;
     public final int score;
 
     private Tile(char letter, int score) {
+        super();
         this.letter = letter;
         this.score = score;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tile tile = (Tile) o;
-        return letter == tile.letter && score == tile.score;
     }
 
     @Override
@@ -26,100 +20,100 @@ public class Tile {
         return Objects.hash(letter, score);
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tile other = (Tile) obj;
+        return letter == other.letter && score == other.score;
+    }
+
     public static class Bag{
-        private static Bag _instance = null;
-        int tilesAmount[] = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1};
-        int legalAmount[] = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1};
-        Tile[] tiles = {new Tile('A', 1), new Tile('B', 3), new Tile('C', 3), new Tile('D', 2), new Tile('E', 1), new Tile('F', 4), new Tile('G', 2), new Tile('H', 4), new Tile('I', 1), new Tile('J', 8), new Tile('K', 5), new Tile('L', 1), new Tile('M', 3), new Tile('N', 1), new Tile('O', 1), new Tile('P', 3), new Tile('Q', 10), new Tile('R', 1), new Tile('S', 1), new Tile('T', 1), new Tile('U', 1), new Tile('V', 4), new Tile('W', 4), new Tile('X', 8), new Tile('Y', 4), new Tile('Z', 10)};
-        int bagCount = 98;
+        private int[] maxQuantities = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1};
+        private int[] quantities = maxQuantities.clone();
+        private Tile[] tiles = {
+                new Tile('A',1),
+                new Tile('B',3),
+                new Tile('C',3),
+                new Tile('D',2),
+                new Tile('E',1),
+                new Tile('F',4),
+                new Tile('G',2),
+                new Tile('H',4),
+                new Tile('I',1),
+                new Tile('J',8),
+                new Tile('K',5),
+                new Tile('L',1),
+                new Tile('M',3),
+                new Tile('N',1),
+                new Tile('O',1),
+                new Tile('P',3),
+                new Tile('Q',10),
+                new Tile('R',1),
+                new Tile('S',1),
+                new Tile('T',1),
+                new Tile('U',1),
+                new Tile('V',4),
+                new Tile('W',4),
+                new Tile('X',8),
+                new Tile('Y',4),
+                new Tile('Z',10)
+        };
 
-        private Bag(){}
+        Random r;
+        int size;
 
-        public Tile getRand(){
-
-            if(bagCount == 0)
-                return null;
-
-            int rand = (int)(Math.random()*26);
-            while(true){
-                if(tilesAmount[rand] > 0){
-                    tilesAmount[rand]--;
-                    bagCount--;
-                    return tiles[rand];
-                }
-                rand++;
-                if(rand == 26){
-                    rand = 0;
-                }
-            }
+        private Bag() {
+            r = new Random();
+            size=98;
         }
 
-        public Tile getTile(char letter){
-
-            if(letter < 65 || letter > 90) return null;
-
-            int tileIdx = letter - 'A';
-            if(tilesAmount[tileIdx] > 0){
-                tilesAmount[tileIdx]--;
-                bagCount--;
-                return tiles[tileIdx];
+        public Tile getRand() {
+            if(size>0) {
+                int i=r.nextInt(quantities.length);
+                while(quantities[i]==0)
+                    i=r.nextInt(quantities.length);
+                size-=1;
+                quantities[i]-=1;
+                return tiles[i];
             }
             return null;
         }
 
-        public void put(Tile t){
-
-            if(t.letter < 65 || t.letter > 90) return;
-
-            int tileIdx = t.letter - 'A';
-            tilesAmount[tileIdx]++;
-            bagCount++;
-            if(tilesAmount[tileIdx] > legalAmount[tileIdx]){
-                tilesAmount[tileIdx] = legalAmount[tileIdx];
-                bagCount--;
-                return;
+        public Tile getTile(char c) {
+            if(c>='A' && c<='Z' && quantities[c-'A']>0) {
+                quantities[c-'A']-=1;
+                size-=1;
+                //System.out.println("getting "+c+" - "+quantities[c-'A']);
+                return tiles[c-'A'];
             }
+            return null;
         }
 
-        public int size(){return bagCount;}
-
-        int[] getQuantities(){
-            int[] copy = new int[26];
-            System.arraycopy(this.tilesAmount,0,copy,0,26);
-            return copy;
+        public int size() {
+            return size;
         }
 
+        public void put(Tile t) {
+            int i=t.letter-'A';
+            if(quantities[i]<maxQuantities[i])
+                quantities[i]+=1;
+        }
+
+        public int[] getQuantities() {
+            return quantities.clone();
+        }
+
+        private static Bag bag=null;
         public static Bag getBag() {
-            if(_instance == null){
-                _instance = new Bag();
-            }
-
-            return _instance;
+            if(bag==null)
+                bag=new Bag();
+            return bag;
         }
-
-
     }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
