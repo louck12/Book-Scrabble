f2add152db2cd838c6b8e1db291154c6e36d86b5
Orel Matatov <orel.matatov@gmail.com>
2023-05-27 19:33:29 +0300

    Update Word.java

--- src/main/java/model/board/Word.java
+++ src/main/java/model/board/Word.java
@@ -1,22 +1,24 @@
+
 package model.board;
 
-
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Word {
-    private final Tile[] tiles;
-    private final int row;
-    private final int col;
-    private final boolean vertical;
+
+    private Tile[] tiles;
+    private int row,col;
+    private boolean vertical;
 
     public Word(Tile[] tiles, int row, int col, boolean vertical) {
+        super();
         this.tiles = tiles;
         this.row = row;
         this.col = col;
         this.vertical = vertical;
     }
 
-    public Tile[] getWord() {
+    public Tile[] getTiles() {
         return tiles;
     }
 
@@ -28,21 +30,38 @@ public class Word {
         return col;
     }
 
-    public boolean getVertical(){return vertical;}
-
     public boolean isVertical() {
         return vertical;
     }
 
-
-
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Word word = (Word) o;
-        return getRow() == word.getRow() && getCol() == word.getCol() && isVertical() == word.isVertical() && Arrays.equals(tiles, word.tiles);
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(tiles);
+        result = prime * result + Objects.hash(col, row, vertical);
+        return result;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Word other = (Word) obj;
+        return col == other.col && row == other.row && Arrays.equals(tiles, other.tiles) && vertical == other.vertical;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb=new StringBuilder();
+        for(Tile t : tiles)
+            sb.append(t.letter);
+        sb.append(" ("+row+","+col+") "+(vertical?"vertical":"horizontal"));
+        return sb.toString();
+    }
 
 }
