13c5c9f3b9f5c4d6e2f20da1f9942ab0a5206e26
Orel Matatov <orel.matatov@gmail.com>
2023-05-27 16:16:48 +0300

    Update HostTest.java

--- src/main/java/test/HostTest.java
+++ src/main/java/test/HostTest.java
@@ -1,9 +1,34 @@
 package test;
 
+import model.Guest;
+import model.Host;
+import model.server.BookScrabbleHandler;
+import model.server.MyServer;
+
+
+
 public class HostTest {
 
     public static void main(String[] args) {
-        //Checking connection to the main server
+
+        MyServer server=new MyServer(1234, new BookScrabbleHandler());
+        System.out.println("Server is running...");
+        server.start();
+
+        Host hostServer = new Host(8000); //8000 is the port connection to the guests
+
+        hostServer.connectToMainServer(); //connection to the main server
+
+        new Thread(() -> {
+            hostServer.hostMode(); //open local server socket
+        }).start();
+
+        Guest g1 = new Guest();
+        Guest g2 = new Guest();
+        Guest g3 = new Guest();
+        Guest g4 = new Guest();
+        g1.closeEverything();g2.closeEverything();g3.closeEverything();
+
 
     }
 }
