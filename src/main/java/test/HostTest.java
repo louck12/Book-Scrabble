1febac744e08409cf5c995067aeaa633088c42b5
Orel Matatov <orel.matatov@gmail.com>
2023-05-27 19:28:51 +0300

    Update HostTest.java

--- src/main/java/test/HostTest.java
+++ src/main/java/test/HostTest.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package test;
 
 import model.Guest;
@@ -5,30 +10,30 @@ import model.Host;
 import model.server.BookScrabbleHandler;
 import model.server.MyServer;
 
-
-
 public class HostTest {
+    public HostTest() {
+    }
 
     public static void main(String[] args) {
-
-        MyServer server=new MyServer(1234, new BookScrabbleHandler());
+        MyServer server = new MyServer(1234, new BookScrabbleHandler());
         System.out.println("Server is running...");
         server.start();
-
-        Host hostServer = new Host(8000); //8000 is the port connection to the guests
-
-        hostServer.connectToMainServer(); //connection to the main server
-
-        new Thread(() -> {
-            hostServer.hostMode(); //open local server socket
-        }).start();
-
+        Host hostServer = new Host(8000);
+        hostServer.connectToMainServer();
+        (new Thread(() -> {
+            hostServer.hostMode();
+        })).start();
         Guest g1 = new Guest();
+        g1.connectToHost();
         Guest g2 = new Guest();
+        g2.connectToHost();
         Guest g3 = new Guest();
+        g3.connectToHost();
         Guest g4 = new Guest();
-        g1.closeEverything();g2.closeEverything();g3.closeEverything();
-
-
+        g4.connectToHost();
+        g1.closeEverything();
+        g2.closeEverything();
+        g3.closeEverything();
+        hostServer.closeEverything();
     }
 }
