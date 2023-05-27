8010f4552b1a22322051b71797b7967dcb80c99f
Orel Matatov <orel.matatov@gmail.com>
2023-05-27 19:34:09 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,20 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 module com.example.clientside {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens view to javafx.fxml;
     exports view;
     exports model;
-    opens model to javafx.fxml;
+    exports test;
+
+    opens view to
+            javafx.fxml;
+    opens model to
+            javafx.fxml;
+    opens test to
+            javafx.fxml;
 }
