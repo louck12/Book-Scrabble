5ebc80158fe0a69afbb3d674a531ca9cc9347d44
Orel Matatov <orel.matatov@gmail.com>
2023-06-18 19:45:55 +0300

    Add listening thread to Guest Class

--- src/main/java/model/Guest.java
+++ src/main/java/model/Guest.java
@@ -1,9 +1,13 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
 
 package model;
+
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import model.server.BookScrabbleHandler;
+import model.server.MyServer;
+import view.HelloApplication;
+import view.WindowController;
+import viewModel.ViewModel;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,29 +21,53 @@ public class Guest {
     Scanner inFromHost;
     PrintWriter outToHost;
     public int numOfWords = 0;
-    Facade gf = new GuestFacade(this);
+
+    WindowController c;
+
     public ArrayList<String> l = new ArrayList();
 
-    public Guest() {
+    public Guest(WindowController c) {
+        this.c = c;
     }
 
     public void connectToHost() {
+
+        if(socket == null){
+            try{
+                Thread.sleep(3000);
+            }catch (InterruptedException e){}
+        }
+
         try {
-            this.socket = new Socket("127.0.0.1", 8000);
+            this.socket = new Socket("localhost", 8080);
             this.inFromHost = new Scanner(this.socket.getInputStream());
             this.outToHost = new PrintWriter(this.socket.getOutputStream());
-        } catch (IOException var2) {
-            var2.printStackTrace();
-        } catch (NoSuchElementException var3) {
+            System.out.println("Listening to the host...");
+
+        } catch (IOException e) {
+            System.out.println("cannot connect");
+        } catch (NoSuchElementException e1) {
             System.out.println("Cannot connect");
         }
+
+        new Thread(()->{
+            while(socket.isConnected()){
+                String ans = inFromHost.nextLine();
+               if(ans.contains("[usernames]")){
+                    String unames = ans.split(":")[1];
+                    Platform.runLater(()->c.addUserName(unames));
+                }
+                else{
+                    Platform.runLater(() -> c.handleAnswer(ans));
+                }
+            }
+        }).start();
 
     }
 
     public void sendWordToHost(String word) {
         this.outToHost.println(word);
         this.outToHost.flush();
-        ++this.numOfWords;
     }
 
     public void challengeWord(String word) {
@@ -54,8 +82,9 @@ public class Guest {
             this.l.add(ans);
             System.out.println(ans);
         }
-
     }
+
+
 
     public void closeEverything() {
         try {
@@ -70,8 +99,10 @@ public class Guest {
             if (this.outToHost != null) {
                 this.outToHost.close();
             }
-        } catch (IOException var2) {
-            var2.printStackTrace();
+
+            System.out.println("Guest just disconnected");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
