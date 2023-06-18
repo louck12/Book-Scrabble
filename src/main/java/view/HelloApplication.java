e4f0be97787d32257523c0fc6fc19099d2eb0ebe
Orel Matatov <orel.matatov@gmail.com>
2023-06-18 19:57:38 +0300

    Update HelloApplication

--- src/main/java/view/HelloApplication.java
+++ src/main/java/view/HelloApplication.java
@@ -3,21 +3,32 @@ package view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.Host;
+import model.Model;
+import model.server.BookScrabbleHandler;
+import model.server.MyServer;
+import viewModel.ViewModel;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        BorderPane borderPane = fxmlLoader.load();
+        Scene scene = new Scene(borderPane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         launch();
+
     }
 }
