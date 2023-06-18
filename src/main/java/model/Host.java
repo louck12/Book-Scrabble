635c4181d71d461a42cad7895e7363ac9477245f
Orel Matatov <orel.matatov@gmail.com>
2023-06-18 19:49:05 +0300

    Spreading new usernames across all guests

--- src/main/java/model/Host.java
+++ src/main/java/model/Host.java
@@ -5,23 +5,31 @@
 
 package model;
 
+import viewModel.ViewModel;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Host {
     private static final int MAX_GUESTS = 3;
-    public String ip = "localhost";
+    public String ip = "10.160.2.54";
     public int port;
     PrintWriter outToServer;
     Scanner inFromServer;
     Socket socket;
-    ServerSocket serverSocket;
-    public int check = 0;
+
+    TurnManager turnManager;
+
     public static ArrayList<GuestHandler> guestHandlers = new ArrayList();
+
+    String usernames = "[usernames]:";
+
 
     public Host(int port) {
         this.port = port;
@@ -40,35 +48,50 @@ public class Host {
 
     public void hostMode() {
         try {
-            this.serverSocket = new ServerSocket(this.port);
+            ServerSocket serverSocket = new ServerSocket(this.port);
             System.out.println("Host Server Is Running...");
+            turnManager = new TurnManager(guestHandlers);
 
-            for(int i = 0; i < 3; ++i) {
-                Socket guestSocket = this.serverSocket.accept();
+            for(int i = 0; i < MAX_GUESTS; ++i) {
+                Socket guestSocket = serverSocket.accept();
                 System.out.println("A Guest Just Connected");
-                this.check = 1;
-                (new Thread(() -> {
-                    try {
-                        GuestHandler ch = new GuestHandler(this.inFromServer, this.outToServer, guestSocket);
-                        guestHandlers.add(ch);
-                        ch.handleClient(guestSocket.getInputStream(), guestSocket.getOutputStream());
-                    } catch (IOException var3) {
-                        throw new RuntimeException(var3);
-                    }
-                })).start();
+                GuestHandler ch = new GuestHandler(this.inFromServer, this.outToServer, guestSocket, this);
+                guestHandlers.add(ch);
+
+                Scanner s = new Scanner(guestSocket.getInputStream());
+                String username = s.nextLine();
+
+                turnManager.addGuestToQueue(username);
+                usernames+=username;
+                usernames+=",";
+
+                System.out.println(usernames);
+                for(GuestHandler gh: guestHandlers){
+                   PrintWriter w =  new PrintWriter(gh.guestSocket.getOutputStream());
+                   w.println(usernames);
+                   w.flush();
+                }
+
+                new Thread(() -> {
+                        try {
+                            ch.handleClient(guestSocket.getInputStream(), guestSocket.getOutputStream());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).start();
             }
 
-            while(!this.serverSocket.isClosed()) {
-                Socket blockedGuestSocket = this.serverSocket.accept();
-                this.check = 0;
+            while(!serverSocket.isClosed()) {
+                Socket blockedGuestSocket = serverSocket.accept();
                 System.out.println("Blocked additional guest from connecting");
                 blockedGuestSocket.close();
             }
-        } catch (IOException var3) {
+        } catch (IOException e) {
             System.out.println("Something went wrong with the server socket");
         }
 
     }
+
 
     public void closeEverything() {
         try {
@@ -83,14 +106,17 @@ public class Host {
             if (this.outToServer != null) {
                 this.outToServer.close();
             }
-        } catch (IOException var2) {
-            var2.printStackTrace();
+
+
+            System.out.println("the host disconnected");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
 
     public static void main(String[] args) {
-        Host host = new Host(8000);
-        host.hostMode();
+
     }
+
 }
