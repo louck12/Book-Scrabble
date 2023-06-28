d03acbb97afef533bc6c81901dc37c0a5d885f57
Orel Matatov <orel.matatov@gmail.com>
2023-06-28 17:18:48 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,11 +10,14 @@ module com.example.clientside {
     exports view;
     exports model;
     exports test;
+    exports viewModel;
 
     opens view to
             javafx.fxml;
     opens model to
             javafx.fxml;
     opens test to
+            javafx.fxml;
+    opens viewModel to
             javafx.fxml;
 }
